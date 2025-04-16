e95b53d4153a76c393a4757c9cc2346bcb9b4dd5
Teo_You <teoyou881@gmail.com>
2025-04-15 23:34:41 -0400

    test code for commit & rollback without transaction.

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -3,6 +3,7 @@ package hello.springtx.propagation;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,5 +32,21 @@ class MemberServiceTest {
                                .isPresent());
     assertTrue(logRepository.find(username)
                             .isPresent());
+  }
+
+  @Test
+  void outerTxOff_fail() {
+    // given
+    String username = "LogException";
+
+    // when
+    Assertions.assertThatThrownBy(() -> memberService.joinV1(username))
+              .isInstanceOf(RuntimeException.class);
+
+    // then: member data was saved, but log data was not.
+    assertTrue(memberRepository.find(username)
+                               .isPresent());
+    assertTrue(logRepository.find(username)
+                            .isEmpty());
   }
 }
